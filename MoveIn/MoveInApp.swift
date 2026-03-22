e1bea659dd8015d2e9b4a29b2c0f75efb32e1601
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-03-22 09:06:04 -0400

    Initial Commit

---
+++ MoveIn/MoveInApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveInApp.swift
+//  MoveIn
+//
+//  Created by Ayesha Akbar on 2026-03-22.
+//
+
+import SwiftUI
+
+@main
+struct MoveInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
