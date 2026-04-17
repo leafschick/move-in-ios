791bb8d36fd7076f4b5bf03363cc809ffae1072f
Lakshay Dhawan <lakshay@Lakshays-MacBook-Air.local>
2026-04-17 16:17:55 -0400

    fix(ui): remove outer NavigationStack to prevent nested navigation toolbars from disappearing

--- MoveIn/AppFlowView.swift
+++ MoveIn/AppFlowView.swift
@@ -14,78 +14,76 @@ struct AppFlowView: View {
     @State private var currentScreen: Screen = .landing
 
     var body: some View {
-        NavigationStack {
-            Group {
-                switch currentScreen {
-                case .landing:
-                    LandingView(
-                        onLoginTap: {
-                            currentScreen = .login
-                        },
-                        onSignUpTap: {
-                            currentScreen = .signup
-                        }
-                    )
+        Group {
+            switch currentScreen {
+            case .landing:
+                LandingView(
+                    onLoginTap: {
+                        currentScreen = .login
+                    },
+                    onSignUpTap: {
+                        currentScreen = .signup
+                    }
+                )
 
-                case .login:
-                    LoginView(
-                        onCustomerLogin: {
-                            currentScreen = .customerLogin
-                        },
-                        onVendorLogin: {
-                            currentScreen = .vendorLogin
-                        },
-                        onShowSignUp: {
-                            currentScreen = .signup
-                        },
-                        onBack: {
-                            currentScreen = .landing
-                        }
-                    )
+            case .login:
+                LoginView(
+                    onCustomerLogin: {
+                        currentScreen = .customerLogin
+                    },
+                    onVendorLogin: {
+                        currentScreen = .vendorLogin
+                    },
+                    onShowSignUp: {
+                        currentScreen = .signup
+                    },
+                    onBack: {
+                        currentScreen = .landing
+                    }
+                )
 
-                case .signup:
-                    SignUpView(
-                        onBack: {
-                            currentScreen = .landing
-                        },
-                        onShowLogin: {
-                            currentScreen = .login
-                        }
-                    )
+            case .signup:
+                SignUpView(
+                    onBack: {
+                        currentScreen = .landing
+                    },
+                    onShowLogin: {
+                        currentScreen = .login
+                    }
+                )
 
-                case .customerLogin:
-                    CustomerLoginView(
-                        onLoginSuccess: {
-                            currentScreen = .home
-                        },
-                        onBack: {
-                            currentScreen = .login
-                        },
-                        onShowSignUp: {
-                            currentScreen = .signup
-                        }
-                    )
+            case .customerLogin:
+                CustomerLoginView(
+                    onLoginSuccess: {
+                        currentScreen = .home
+                    },
+                    onBack: {
+                        currentScreen = .login
+                    },
+                    onShowSignUp: {
+                        currentScreen = .signup
+                    }
+                )
 
-                case .vendorLogin:
-                    VendorLoginView(
-                        onLoginSuccess: {
-                            currentScreen = .vendorDashboard
-                        },
-                        onBack: {
-                            currentScreen = .login
-                        }
-                    )
+            case .vendorLogin:
+                VendorLoginView(
+                    onLoginSuccess: {
+                        currentScreen = .vendorDashboard
+                    },
+                    onBack: {
+                        currentScreen = .login
+                    }
+                )
 
-                case .home:
-                    MainTabView()
+            case .home:
+                MainTabView()
 
-                case .vendorDashboard:
-                    VendorDashboardView(
-                        onLogout: {
-                            currentScreen = .landing
-                        }
-                    )
-                }
+            case .vendorDashboard:
+                VendorDashboardView(
+                    onLogout: {
+                        currentScreen = .landing
+                    }
+                )
             }
         }
         // ✅ THIS IS THE FIX (listen for logout from HomeView)
